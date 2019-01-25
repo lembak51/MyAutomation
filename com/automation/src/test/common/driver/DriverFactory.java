19d4dc02160006f0a4fc72158845152e5a32f397
Tomash Gombosh <tomash.gombosh@swanlogic.com>
2019-01-25 14:16:13 +0200

    Add browser capabilities

--- com/automation/src/test/common/driver/DriverFactory.java
+++ com/automation/src/test/common/driver/DriverFactory.java
@@ -5,14 +5,24 @@ import common.Utils;
 import org.apache.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.firefox.FirefoxProfile;
 import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.ie.InternetExplorerOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.safari.SafariOptions;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 public class DriverFactory {
     private String browser;
     private WebDriver driver;
     private Logger log = Logger.getLogger(this.getClass().getSimpleName());
+    private DesiredCapabilities capabilities = null;
 
     public DriverFactory(){
         this.browser = new ProjectConfig().getBrowser();
@@ -26,7 +36,8 @@ public class DriverFactory {
     private void setUpDriver(String browser){
         if (browser.equalsIgnoreCase("Chrome")) {
             initChromeDriverPath();
-            driver = new ChromeDriver();
+            setChromeCapabilities();
+            driver = new ChromeDriver(capabilities);
         } else if (browser.equalsIgnoreCase("IE")) {
             initInternetExplorerDriverPath();
             driver = new InternetExplorerDriver();
@@ -35,6 +46,9 @@ public class DriverFactory {
         driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
     }
 
+    /**
+     * initChromeDriverPath method initialize chrome driver on following OS
+     */
     private void initChromeDriverPath(){
         String chromeDriverPath = System.getProperty("user.dir")+  "/com/automation/src/resources/drivers";
 
@@ -46,7 +60,9 @@ public class DriverFactory {
             chromeDriverPath += "/linux/chromedriver";
         }
         System.setProperty("webdriver.chrome.driver", chromeDriverPath);
+
     }
+
 
     private void initInternetExplorerDriverPath(){
         String internetExplorerPath = System.getProperty("user.dir") + "/com/automation/src/resources/drivers";
@@ -60,4 +76,69 @@ public class DriverFactory {
         }
         System.setProperty("webdriver.ie.driver", internetExplorerPath);
     }
+
+
+    /**
+     * setChromeCapabilities method set chrome capabilities to the web driver
+     */
+    private void setChromeCapabilities(){
+        capabilities = DesiredCapabilities.chrome();
+
+        ChromeOptions chOptions = new ChromeOptions();
+        Map<String, Object> chromePreferences = new HashMap<String, Object>();
+
+        chromePreferences.put("credentials_enable_service", false);
+        chOptions.setExperimentalOption("prefs", chromePreferences);
+        chOptions.addArguments("--disable-plugins",
+                "--disable-extensions",
+                "--disable-popup-blocking");
+
+        capabilities.setCapability(ChromeOptions.CAPABILITY, chOptions);
+        capabilities.setCapability("applicationCacheEnable", false);
+    }
+
+    /**
+     * setFirefoxCapabilities method set Firefox capabilities to the web driver
+     */
+    private void setFirefoxCapabilities(){
+        capabilities = DesiredCapabilities.firefox();
+
+        FirefoxOptions firefoxOptions = new FirefoxOptions();
+        FirefoxProfile firefoxProfile = new FirefoxProfile();
+
+        firefoxProfile.setPreference("browser.autofocus",true);
+        firefoxProfile.setPreference("dom.disable_open_during_load", false);
+        capabilities.setCapability(FirefoxDriver.PROFILE, firefoxProfile);
+        capabilities.setCapability("marionette", true);
+    }
+
+    /**
+     * setInternetExplorerCapabilities method set Internet Explorer capabilities to the web driver
+     */
+    private void setInternetExplorerCapabilities(){
+        capabilities = DesiredCapabilities.internetExplorer();
+
+        InternetExplorerOptions ieOptions = new InternetExplorerOptions();
+
+        ieOptions.requireWindowFocus();
+        ieOptions.merge(capabilities);
+        capabilities.setCapability("requireWindowFocus", true);
+    }
+
+
+    /**
+     * setSafariCapabilities method set Safari capabilities to the web driver
+     */
+    private void setSafariCapabilities(){
+        capabilities = DesiredCapabilities.safari();
+
+        SafariOptions safariOptions = new SafariOptions();
+        safariOptions.setUseCleanSession(true);
+
+        capabilities.setCapability(SafariOptions.CAPABILITY,safariOptions);
+        capabilities.setCapability("autoAcceptAlerts",true);
+        //TODO implementation to add commands to command line from disable pop-up blocking
+    }
+
+
 }
