6ba5be5486c581b4c51e42f6cf51bd5186c1cbbe
bogdanron <bogdan.velichkanich@swanlogic.com>
2019-02-07 14:59:52 +0200

    WIP phonebookTest

--- com/automation/src/test/pages/webPages/phonebookPages/UserListingPage.java
+++ com/automation/src/test/pages/webPages/phonebookPages/UserListingPage.java
@@ -1,6 +1,7 @@
 package pages.webPages.phonebookPages;
 
 import common.PageElement;
+import common.dataObjects.PhonebookDataObject;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import pages.webPages.BasePage;
@@ -8,7 +9,7 @@ import pages.webPages.BasePage;
 public class UserListingPage extends BasePage {
     private static final PageElement mobileArrowBtn = new PageElement(
             "Mobile Arrow button",
-            By.cssSelector("sort-chevron[data-text='Mobile'] > span > span.fa.cursor"), //need to change: sort-chevron[data-text='Mobile']>span>span[1]
+            By.cssSelector("sort-chevron[data-text='Mobile'] > span > span.fa.cursor"),
             true);
     private static final PageElement firstElementInMobileColumnMsb = new PageElement(
             "First element in Mobile column",
@@ -16,7 +17,7 @@ public class UserListingPage extends BasePage {
             true);
     private static final PageElement didArrowBtn = new PageElement(
             "DID Arrow button",
-            By.cssSelector("sort-chevron[data-text='DID'] > span > span.fa.cursor"), //need to change: sort-chevron[data-text='Mobile']>span>span[1]
+            By.cssSelector("sort-chevron[data-text='DID'] > span > span.fa.cursor"),
             true);
     private static final PageElement firstElementInDidColumnMsb = new PageElement(
             "First element in DID column",
@@ -24,7 +25,7 @@ public class UserListingPage extends BasePage {
             true);
     private static final PageElement departmentArrowBtn = new PageElement(
             "Department(s) Arrow button",
-            By.cssSelector("sort-chevron[data-text='Department(s)'] > span > span.fa.cursor"), //need to change: sort-chevron[data-text='Mobile']>span>span[1]
+            By.cssSelector("sort-chevron[data-text='Department(s)'] > span > span.fa.cursor"),
             true);
     private static final PageElement firstElementInDepartmentColumnMsb = new PageElement(
             "First element in Department(s) column",
@@ -32,12 +33,36 @@ public class UserListingPage extends BasePage {
             true);
     private static final PageElement extensionArrowBtn = new PageElement(
             "Extension(s) Arrow button",
-            By.cssSelector("sort-chevron[data-text='Extension(s)'] > span > span.fa.cursor"), //need to change: sort-chevron[data-text='Mobile']>span>span[1]
+            By.cssSelector("sort-chevron[data-text='Extension(s)'] > span > span.fa.cursor"),
             true);
     private static final PageElement firstElementInExtensionColumnMsb = new PageElement(
             "First element in Extension(s) column",
             By.cssSelector("td:nth-child(6) > span > call > span.ng-scope > span"),
             true);
+    private static final PageElement firstNameinTableMsb = new PageElement(
+            "First name in table",
+            By.xpath("//tr[1]/td[1]/strong"),
+            false);
+    private static final PageElement firstEmailInTableMsb = new PageElement(
+            "First email in the table",
+            By.xpath("//tr[1]/td[2]/a"),
+            false);
+    private static final PageElement firstNumberInTableMsb = new PageElement(
+            "First number in the table",
+            By.xpath("//tr[1]/td[3]/call/span[2]/span"),
+            false);
+    private static final PageElement firstNameinPopUpMsb = new PageElement(
+            "First name in pop-up",
+            By.xpath("//div[2]/div/div[1]/div[2]/span[1]"),
+            false);
+    private static final PageElement firstEmailInPopUpMsb = new PageElement(
+            "First email in the pop-up",
+            By.xpath("//div/strong/a"),
+            false);
+    private static final PageElement firstNumberInPopUpMsb = new PageElement(
+            "First number in the pop-up",
+            By.xpath("//strong/strong/span"),
+            false);
 
 
     public UserListingPage(WebDriver driver){
@@ -80,4 +105,45 @@ public class UserListingPage extends BasePage {
     public boolean sortExtensionElementsByDesc(){
         return isElementsSorted(extensionArrowBtn, firstElementInExtensionColumnMsb, "By desc");
     }
+
+    public String getNameTextFromTheTable(){
+        return getText(firstNameinTableMsb);
+    }
+
+    public String getEmailTextFromTheTable(){
+        return getText(firstEmailInTableMsb);
+    }
+
+    public String getNumberTextFromTheTable(){
+        return getText(firstNumberInTableMsb);
+    }
+
+    public String getNameTextFromThePopUp(){
+        return getText(firstNameinPopUpMsb);
+    }
+
+    public String getEmailTextFromThePopUp(){
+        return getText(firstEmailInPopUpMsb);
+    }
+
+    public String getNumberTextFromThePopUp(){
+        return getText(firstNumberInPopUpMsb);
+    }
+
+    public PhonebookDataObject getValuesFromTheTable(){
+        PhonebookDataObject expectedDataObject = new PhonebookDataObject();
+        expectedDataObject.Name = getNameTextFromTheTable();
+        expectedDataObject.Email = getEmailTextFromTheTable();
+        expectedDataObject.Number = getNumberTextFromTheTable();
+        return expectedDataObject;
+    }
+
+    public PhonebookDataObject getValuesFromPopUp(){
+        PhonebookDataObject actualDataObject = new PhonebookDataObject();
+        actualDataObject.Name = getNameTextFromThePopUp();
+        actualDataObject.Email = getEmailTextFromThePopUp();
+        actualDataObject.Number = getNumberTextFromThePopUp();
+        return actualDataObject;
+    }
+
 }
