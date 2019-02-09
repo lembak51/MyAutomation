a5ea13adfe96ef6d231d04c7eb66a4caf72f9a42
Kristian Gombosh <kristian.hombosh@swanteams.com>
2019-02-09 23:15:23 +0200

    fixed merge conflict

--- com/automation/src/test/pages/webPages/BasePage.java
+++ com/automation/src/test/pages/webPages/BasePage.java
@@ -19,7 +19,7 @@ abstract class BasePage {
     private WebDriver driver;
     protected Logger log;
 
-    BasePage(WebDriver driver) {
+    BasePage(WebDriver driver){
         this.driver = driver;
         log = Logger.getLogger(this.getClass().getCanonicalName());
     }
@@ -27,16 +27,16 @@ abstract class BasePage {
     public abstract boolean pageIsDisplayed();
 
 
-    public String getText(PageElement pageElement) {
+    public String getText(PageElement pageElement){
         log.info("Getting text of element: " + pageElement.name);
         return this.find(pageElement).getText();
     }
 
-    public void enterText(PageElement pageElement, String text) {
+    public void enterText(PageElement pageElement, String text){
         this.enterText(pageElement, text, true);
     }
 
-    public void enterText(PageElement pageElement, String text, boolean clearField) {
+    public void enterText(PageElement pageElement, String text, boolean clearField){
         log.info("Entering text \"" + text + "\" to element: " + pageElement.name);
         this.find(pageElement).click();
         if (clearField) {
@@ -45,24 +45,24 @@ abstract class BasePage {
         this.find(pageElement).sendKeys(text);
     }
 
-    public void click(PageElement pageElement) {
+    public void click(PageElement pageElement){
         log.info("Clicking on element: " + pageElement.name);
         this.find(pageElement).click();
     }
 
-    public WebElement find(By element) {
+    public WebElement find(By element){
         return this.driver.findElement(element);
     }
 
-    public WebElement find(PageElement element) {
+    public WebElement find(PageElement element){
         return this.find(element.getLocator());
     }
 
-    public List<WebElement> findAll(By element) {
+    public List<WebElement> findAll(By element){
         return this.driver.findElements(element);
     }
 
-    public List<WebElement> findAll(PageElement element) {
+    public List<WebElement> findAll(PageElement element){
         return this.findAll(element.getLocator());
     }
 
@@ -71,7 +71,7 @@ abstract class BasePage {
      *
      * @return a list of all the elements in this object.
      */
-    public List<PageElement> getElements() {
+    public List<PageElement> getElements(){
         List<PageElement> elements = new ArrayList<PageElement>();
         for (Field field : this.getClass().getDeclaredFields()) {
             if (field.getType().getSimpleName().equals("PageElement")) {
@@ -92,7 +92,7 @@ abstract class BasePage {
      *
      * @return an array of the required elements on this page.
      */
-    public List<PageElement> getRequiredElements() {
+    public List<PageElement> getRequiredElements(){
         ArrayList<PageElement> requiredElements = new ArrayList<PageElement>();
         for (PageElement ele : this.getElements()) {
             if (ele.required) {
@@ -107,7 +107,7 @@ abstract class BasePage {
      *
      * @return all the elements in the array that were not visible.
      */
-    public ArrayList<PageElement> getMissingRequiredElements(List<PageElement> requiredElements) {
+    public ArrayList<PageElement> getMissingRequiredElements(List<PageElement> requiredElements){
         ArrayList<PageElement> elements = new ArrayList<PageElement>(requiredElements);
         for (PageElement ele : requiredElements) {
             if (this.isElementPresent(ele)) {
@@ -119,12 +119,12 @@ abstract class BasePage {
         return elements;
     }
 
-    protected boolean allRequiredElementDisplayed() {
+    protected boolean allRequiredElementDisplayed(){
         log.info("Checking if all required elements present on page");
         return this.getMissingRequiredElements(this.getRequiredElements()).isEmpty();
     }
 
-    public boolean isElementPresent(By element) {
+    public boolean isElementPresent(By element){
         boolean elementFound;
         try {
             this.find(element);
@@ -135,7 +135,7 @@ abstract class BasePage {
         return elementFound;
     }
 
-    public boolean isElementPresent(PageElement element) {
+    public boolean isElementPresent(PageElement element){
         return this.isElementPresent(element.getLocator());
     }
 
@@ -145,27 +145,27 @@ abstract class BasePage {
      * @param element the By object representing the element to wait for.
      * @param timeout the length of time in seconds to wait, as an integer.
      */
-    public void waitToBeVisible(final By element, int timeout) {
+    public void waitToBeVisible(final By element, int timeout){
         Wait<WebDriver> wait = new FluentWait<WebDriver>(this.driver)
                 .withTimeout(timeout, TimeUnit.SECONDS)
                 .pollingEvery(1, TimeUnit.SECONDS)
                 .ignoreAll(Arrays.asList(ElementNotVisibleException.class, NoSuchElementException.class, StaleElementReferenceException.class, WebDriverException.class));
         wait.until(new Function<WebDriver, WebElement>() {
-            public WebElement apply(WebDriver input) {
+            public WebElement apply(WebDriver input){
                 return input.findElement(element);
             }
         });
     }
 
-    public void waitToBeVisible(By element) {
+    public void waitToBeVisible(By element){
         this.waitToBeVisible(element, 30);
     }
 
-    public void waitToBeVisible(PageElement element, int timeout) {
+    public void waitToBeVisible(PageElement element, int timeout){
         this.waitToBeVisible(element.getLocator(), timeout);
     }
 
-    public void waitToBeVisible(PageElement element) {
+    public void waitToBeVisible(PageElement element){
         this.waitToBeVisible(element.getLocator(), 30);
     }
 
@@ -175,21 +175,21 @@ abstract class BasePage {
      * @param element the By object representing the element to wait for.
      * @param timeout the length of time in seconds to wait, as an integer.
      */
-    public void waitToBeInvisible(By element, int timeout) {
+    public void waitToBeInvisible(By element, int timeout){
         WebDriverWait wait = new WebDriverWait(this.driver, timeout);
         wait.until(ExpectedConditions.invisibilityOfElementLocated(element));
     }
 
-    public void waitToBeInvisible(By element) {
+    public void waitToBeInvisible(By element){
         this.waitToBeInvisible(element, 60);
     }
 
-    public void waitToBeInvisible(PageElement element, int timeout) {
+    public void waitToBeInvisible(PageElement element, int timeout){
         log.info("Wait to be invisible " + element.name + " ...");
         this.waitToBeInvisible(element.getLocator(), timeout);
     }
 
-    public void waitToBeInvisible(PageElement element) {
+    public void waitToBeInvisible(PageElement element){
         this.waitToBeInvisible(element.getLocator(), 60);
     }
 
@@ -199,20 +199,20 @@ abstract class BasePage {
      * @param element the By object representing the element to wait for.
      * @param timeout the length of time in seconds to wait, as an integer.
      */
-    public void waitToBeClickable(By element, int timeout) {
+    public void waitToBeClickable(By element, int timeout){
         WebDriverWait wait = new WebDriverWait(this.driver, timeout);
         wait.until(ExpectedConditions.elementToBeClickable(element));
     }
 
-    public void waitToBeClickable(By element) {
+    public void waitToBeClickable(By element){
         this.waitToBeClickable(element, 30);
     }
 
-    public void waitToBeClickable(PageElement element, int timeout) {
+    public void waitToBeClickable(PageElement element, int timeout){
         this.waitToBeClickable(element.getLocator(), timeout);
     }
 
-    public void waitToBeClickable(PageElement element) {
+    public void waitToBeClickable(PageElement element){
         this.waitToBeClickable(element.getLocator(), 30);
     }
 
@@ -222,30 +222,30 @@ abstract class BasePage {
      * @param element the By object representing the element to wait for.
      * @param timeout the length of time in seconds to wait, as an integer.
      */
-    public void waitToBePresent(By element, int timeout) {
+    public void waitToBePresent(By element, int timeout){
         WebDriverWait wait = new WebDriverWait(this.driver, timeout);
         wait.until(ExpectedConditions.presenceOfElementLocated(element));
     }
 
-    public void waitToBePresent(By element) {
+    public void waitToBePresent(By element){
         this.waitToBePresent(element, 30);
     }
 
-    public void waitToBePresent(PageElement element, int timeout) {
+    public void waitToBePresent(PageElement element, int timeout){
         this.waitToBePresent(element.getLocator(), timeout);
     }
 
-    public void waitToBePresent(PageElement element) {
+    public void waitToBePresent(PageElement element){
         this.waitToBePresent(element.getLocator(), 30);
     }
+
     /**
      * Select specific item from drop down
      *
      * @param selectBox the Select object representing the drop down.
-     * @param value the String object that find in the drop down
-     *
+     * @param value     the String object that find in the drop down
      */
-    public void selectFromSelectBox(PageElement selectBox, String value) {
+    public void selectFromSelectBox(PageElement selectBox, String value){
         log.info("Selecting \"" + value + "\" from: " + selectBox.name);
         Select dropdown = new Select(find(selectBox));
         dropdown.selectByVisibleText(value);
@@ -255,9 +255,8 @@ abstract class BasePage {
      * Select random item from drop down
      *
      * @param dropdown the Select object representing the drop down.
-     *
      */
-    public void selectFromDropdown(WebElement dropdown) {
+    public void selectFromDropdown(WebElement dropdown){
         Select listDropdown = new Select(dropdown);
         List<WebElement> list = listDropdown.getOptions();
         listDropdown.selectByIndex(Utils.getRandomInteger(list.size()));
@@ -265,9 +264,8 @@ abstract class BasePage {
 
     /**
      * Waits default(1 sec) timeout period for an alert to be present.
-     *
      */
-    public void waitToBeAlertPresent() {
+    public void waitToBeAlertPresent(){
         try {
             WebDriverWait wait = new WebDriverWait(driver, 1);
             wait.until(ExpectedConditions.alertIsPresent());
@@ -281,7 +279,7 @@ abstract class BasePage {
      *
      * @param timeout the Int object in the second that need to wait
      */
-    public void waitToBeAlertPresent(int timeout) {
+    public void waitToBeAlertPresent(int timeout){
         try {
             WebDriverWait wait = new WebDriverWait(driver, timeout);
             wait.until(ExpectedConditions.alertIsPresent());
@@ -295,7 +293,7 @@ abstract class BasePage {
      *
      * @return Alert Text the String object
      */
-    protected String getTextFromAlert() {
+    protected String getTextFromAlert(){
         waitToBeAlertPresent();
         Alert alert = driver.switchTo().alert();
         String alertText = alert.getText();
@@ -305,9 +303,8 @@ abstract class BasePage {
 
     /**
      * Accept alert
-     *
      */
-    protected void acceptAlert() {
+    protected void acceptAlert(){
         waitToBeAlertPresent();
         driver.switchTo().alert().accept();
     }
@@ -319,9 +316,43 @@ abstract class BasePage {
      * @param attribute the String object representing the value of an attribute of the element
      * @return Attribute value the String object
      */
-    protected String getAttribute(PageElement element, String attribute) {
+    protected String getAttribute(PageElement element, String attribute){
         log.info("Get attribute " + attribute + " from element " + element.name);
         return driver.findElement(element.locator).getAttribute(attribute);
+    }
+
+    /**
+     * Waits default(1 sec) timeout period
+     *
+     * @param timeout the Int object representing the value in second that need to wait
+     */
+    protected void waitUntilPageLoad(int timeout){
+        //TODO implementation of JS to that
+        try {
+            Thread.sleep(timeout * 1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Waits default(1 sec) timeout period to the specific browser
+     *
+     * @param timeout the Int object representing the value in second that need to wait
+     * @param chrome  the Boolean object representing in what browser need wait
+     * @param firefox the Boolean object representing in what browser need wait
+     * @param ie      the Boolean object representing in what browser need wait
+     * @param safari  the Boolean object representing in what browser need wait
+     */
+    protected void waitUntilPageLoad(int timeout, boolean chrome, boolean firefox, boolean ie, boolean safari){
+        if (ProjectConfig.getBrowser().contains("Chrome") && chrome)
+            waitUntilPageLoad(timeout);
+        if (ProjectConfig.getBrowser().contains("Firefox") && firefox)
+            waitUntilPageLoad(timeout);
+        if (ProjectConfig.getBrowser().contains("IExplorer") && ie)
+            waitUntilPageLoad(timeout);
+        if (ProjectConfig.getBrowser().contains("Safari") && safari)
+            waitUntilPageLoad(timeout);
     }
 
     /**
@@ -336,41 +367,5 @@ abstract class BasePage {
         } catch (NegativeArraySizeException e) {
             log.info("New tab not open");
         }
-    }
-
-    /**
-     * Waits default(1 sec) timeout period
-     *
-     * @param timeout  the Int object representing the value in second that need to wait
-     *
-     */
-    protected void waitUntilPageLoad(int timeout) {
-        //TODO implementation of JS to that
-        try {
-            Thread.sleep(timeout*1000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-    }
-
-    /**
-     * Waits default(1 sec) timeout period to the specific browser
-     *
-     * @param timeout  the Int object representing the value in second that need to wait
-     * @param chrome the Boolean object representing in what browser need wait
-     * @param firefox the Boolean object representing in what browser need wait
-     * @param ie the Boolean object representing in what browser need wait
-     * @param safari the Boolean object representing in what browser need wait
-     *
-     */
-    protected void waitUntilPageLoad(int timeout, boolean chrome , boolean firefox, boolean ie, boolean safari) {
-        if (ProjectConfig.getBrowser().contains("Chrome") && chrome)
-            waitUntilPageLoad(timeout);
-        if (ProjectConfig.getBrowser().contains("Firefox") && firefox)
-            waitUntilPageLoad(timeout);
-        if (ProjectConfig.getBrowser().contains("IExplorer") && ie)
-            waitUntilPageLoad(timeout);
-        if (ProjectConfig.getBrowser().contains("Safari") && safari)
-            waitUntilPageLoad(timeout);
     }
 }
